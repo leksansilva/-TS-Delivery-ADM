914b8cb08a0ffaea27731619527650699392a1dd
leksansilva <69767375+leksansilva@users.noreply.github.com>
2021-10-16 13:19:57 -0300

    test not auth

--- src/services/wAuth.js
+++ src/services/wAuth.js
@@ -66,12 +66,12 @@ export default function Wauth({ component: Component, ...rest }) {
           }
         }
       })
-      .catch((err) => {
+      /* .catch((err) => {
         console.log(err.response.status);
         logout();
         setLoading(false);
         setRedirect(true);
-      });
+      }); */
   }
   useEffect(() => {
     verify();
@@ -85,13 +85,10 @@ export default function Wauth({ component: Component, ...rest }) {
     <Route
       {...rest}
       render={(props) =>
-        !redirect ? (
+
           <Component {...props} />
-        ) : (
-          <Redirect
-            to={{ pathname: "/Login", state: { from: props.location } }}
-          />
-        )
+
+
       }
     />
   );
